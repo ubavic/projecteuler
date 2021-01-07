a115318a60b13b006f0cfeda0112a90bd5d65a29
Nikola Ubavić <mm16310@alas.matf.bg.ac.rs>
2021-01-07 20:19:54 +0100

    Problem 031 added

---
+++ 031/031.hs
@@ -0,0 +1,13 @@
+numberOfWays :: Int -> Int -> Int
+numberOfWays 200 _ = 1
+numberOfWays _   8 = 0
+numberOfWays sumOfCoins currentCoin = sum . map (\x -> numberOfWays (sumOfCoins + x) nextCoin) $ sumsOfCurrentCoin
+  where
+      nextCoin = currentCoin + 1
+      sumsOfCurrentCoin = [ x * coin currentCoin | x <- [0 .. 200], x * coin currentCoin <= 200 - sumOfCoins ]
+
+coin :: Int -> Int
+coin n = [1, 2, 5, 10, 20, 50, 100, 200] !! n 
+
+main :: IO ()
+main = print (numberOfWays 0 0)
