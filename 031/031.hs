8628c1c38ad0fb529cb57bedc7ba4f875eae5ebe
Nikola Ubavić <mm16310@alas.matf.bg.ac.rs>
2021-01-07 20:27:40 +0100

    Big optimization

--- 031/031.hs
+++ 031/031.hs
@@ -4,7 +4,7 @@ numberOfWays _   8 = 0
 numberOfWays sumOfCoins currentCoin = sum . map (\x -> numberOfWays (sumOfCoins + x) nextCoin) $ sumsOfCurrentCoin
   where
       nextCoin = currentCoin + 1
-      sumsOfCurrentCoin = [ x * coin currentCoin | x <- [0 .. 200], x * coin currentCoin <= 200 - sumOfCoins ]
+      sumsOfCurrentCoin = [ x * coin currentCoin | x <- [0 .. (div (200 - sumOfCoins) $ coin currentCoin)]]
 
 coin :: Int -> Int
 coin n = [1, 2, 5, 10, 20, 50, 100, 200] !! n 
